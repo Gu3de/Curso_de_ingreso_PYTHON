ef9ebea0412967020c9ba7c0277a4e445d59a456
Patricio Guede <patricioguede7@gmail.com>
2023-07-25 11:46:45 -0300

    realice ej 01 de listas

--- ejercicios/06_listas/ejercicio_01/List_app_01.py
+++ ejercicios/06_listas/ejercicio_01/List_app_01.py
@@ -5,6 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre : Patricio Joaquin
+Apellido : Guede
+
 Al presionar el botón  'Mostrar', se deberán mostrar los números 
 almacenados en el vector lista_datos utilizando Dialog Alert para informar cada elemento.
 '''
@@ -24,7 +27,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        for i in self.lista_datos :
+            alert ("EJ01", i)
+        
         
         
     
