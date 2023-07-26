2dda7e91b6e6faaf9d55cb3aaf715fdbc933e156
Patricio Guede <patricioguede7@gmail.com>
2023-07-26 09:53:42 -0300

    realice ej 05 de listas

--- ejercicios/06_listas/ejercicio_05/List_app_05.py
+++ ejercicios/06_listas/ejercicio_05/List_app_05.py
@@ -5,6 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre : Patricio Joaquin
+Apellido : Guede
+
 Al presionar el botón 'SUMATORIA' se analizará el vector lista_datos a efectos de calcular 
 la suma de todos los valores, la cual deberá ser informada utilizando Dialog Alert.
 '''
@@ -24,7 +27,10 @@ class App(customtkinter.CTk):
 
 
     def btn_calcular_on_click(self):
-        pass
+        suma = 0
+        for i in self.lista_datos :
+            suma += i
+        alert ("EJ05", suma)
     
 if __name__ == "__main__":
     app = App()
